b90080fb24f21016eff0f6220cfad071d4b071d8
Vitus Schneider <vitus@schneider-grin.ch>
2025-12-31 12:29:13 +0100

    added colorchanging for accent color + made large explosions rarer

--- newYear.js
+++ newYear.js
@@ -8,6 +8,15 @@ var randomSpawnCooldown = 0;
 var hueText = Math.floor(Math.random() * 360);
 let showInstructions = true;
 
+var instructions = [
+    "Click anywhere to launch fireworks!",
+    "Cliquez sur l'écran pour lancer des feux d'artifice!",
+    "Klicke, um Feuerwerk zu starten!",
+]
+
+var instructionsIndex = Math.floor(Math.random() * instructions.length);
+var instructionCycleCooldown = 500;
+
 var fireworks = [];
 var particles = [];
 
@@ -43,15 +52,14 @@ class Firework {
         this.x = tx;
         this.y = height;
         this.coordinates = [];
-        this.sizeMain = Math.random()**3 * 1.8 + 0.8;
-        this.sizeAlt = Math.random()**3 * 2 + 1.2;
+        this.sizeMain = Math.random()**6 * 1.7 + 0.8;
+        this.sizeAlt = Math.random()**6 * 1.9 + 1.0;
         for (let i = 0; i < 5; i++) {
             this.coordinates.push([this.x, this.y]);
         }
     }
 
     update(index) {
-        // Update firework position
         this.y -= this.speed;
         this.coordinates.pop();
         this.coordinates.unshift([this.x, this.y]);
@@ -59,12 +67,13 @@ class Firework {
             let hue = Math.floor(Math.random() * 360)
             let lineWidth = Math.floor(Math.random()*3)+3;
             for (let i = 0; i < Math.floor(density*this.sizeMain); i++) {
-                particles.push(new Particle(this.x, this.y, hue,this.sizeMain, lineWidth));
+                particles.push(new Particle(this.x, this.y, hue,this.sizeMain, lineWidth,0));
             }
             hue = Math.floor(Math.random() * 360)
             lineWidth = Math.floor(Math.random()*3)+3;
+            let hueChange = (Math.random()*2-1)**3*4; //x^3 * 2 results in negative values for x<0; dont use exponents where a%2=0
             for (let i = 0; i < Math.floor(density/2*this.sizeAlt); i++) {
-                particles.push(new Particle(this.x, this.y, hue,this.sizeAlt, lineWidth));
+                particles.push(new Particle(this.x, this.y, hue,this.sizeAlt, lineWidth,hueChange));
             }
             fireworks.splice(index, 1);
         }
@@ -74,7 +83,6 @@ class Firework {
         }
     }
     draw() {
-        // Draw firework
         ctx.beginPath();
         ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
         ctx.lineTo(this.x, this.y);
@@ -85,7 +93,7 @@ class Firework {
 
 
 class Particle {
-    constructor(x, y, hue, size, width) {
+    constructor(x, y, hue, size, width, hueChange) {
         this.x = x;
         this.y = y;
         this.coordinates = [];
@@ -100,6 +108,7 @@ class Particle {
         this.decay = Math.random() * 0.015 + 0.005;
         //this.decay = 0;
         this.hue = hue;
+        this.hueChange = hueChange;
         this.lineWidth = width;
     }
 
@@ -110,13 +119,14 @@ class Particle {
         this.coordinates.pop();
         this.coordinates.unshift([this.x, this.y]);
         this.lightness -= this.decay;
+        this.hue = (this.hue + this.hueChange) % 360;
         if (this.lightness <= 0) {
             particles.splice(index, 1);
         }
     }
     draw() {
         ctx.lineWidth = this.lineWidth;
-        ctx.strokeStyle = 'hsl(' + this.hue + ', 100%, ' + (this.lightness * 100) + '%)';
+        ctx.strokeStyle = 'hsl(' + Math.floor(this.hue) + ', 100%, ' + (this.lightness * 100) + '%)';
         ctx.beginPath();
         ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
         ctx.lineTo(this.x, this.y);
@@ -127,7 +137,7 @@ class Particle {
 function loop() {
     ctx.font = "bold 80px 'Arial'";
     ctx.strokeStyle = 'hsl(' + Math.floor(hueText) + ', 100%, 50%)';
-    hueText = (hueText + 0.2) % 360;
+    hueText = (hueText + 0.5) % 360;
     ctx.textAlign = "center";
     ctx.strokeText("Happy New Year!", width / 2, height /5);
 
@@ -135,10 +145,18 @@ function loop() {
         ctx.font = "20px 'Arial'";
         ctx.fillStyle = 'white';
         ctx.textAlign = "center";
-        ctx.fillText("Click anywhere to launch fireworks!", width / 2, height / 5 + 50);
+        instructionCycleCooldown--;
+        if (instructionCycleCooldown <= 0) {
+            instructionCycleCooldown = 500;
+            ctx.fillStyle = 'black';
+            ctx.fillText(instructions[instructionsIndex], width / 2, height / 5 + 50); //drawing a rect would have me get the coords of the rect first. this easier
+            ctx.fillStyle = 'white';
+            instructionsIndex = (instructionsIndex + 1) % instructions.length;
+        }
+        ctx.fillText(instructions[instructionsIndex], width / 2, height / 5 + 50);
     }
 
-    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.065)";
     ctx.fillRect(0, 0, width, height);
 
     ctx.lineWidth = 3;
@@ -176,4 +194,3 @@ function getBustSize() {
 }
 
 loop();
-
